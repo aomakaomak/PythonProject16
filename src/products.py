3e2445f3d0343304e9dbc312a7fe87fbd245838a
Andrei Makarov <aomak@yandex.ru>
2025-09-27 20:45:48 +0300

    готово

--- src/products.py
+++ src/products.py
@@ -10,16 +10,17 @@ class Product:
         self.price = price
         self.quantity = quantity
 
-if __name__ == "__main__":
-    product1 = Product("Cucumber", "Very tasty", 23.5, 10)
-    product2 = Product("Tomato", "Very fresh", 45.2, 20)
 
-    print(product1.name)
-    print(product1.description)
-    print(product1.price)
-    print(product1.quantity)
-
-    print(product2.name)
-    print(product2.description)
-    print(product2.price)
-    print(product2.quantity)
+# if __name__ == "__main__":
+#     product1 = Product("Cucumber", "Very tasty", 23.5, 10)
+#     product2 = Product("Tomato", "Very fresh", 45.2, 20)
+#
+#     print(product1.name)
+#     print(product1.description)
+#     print(product1.price)
+#     print(product1.quantity)
+#
+#     print(product2.name)
+#     print(product2.description)
+#     print(product2.price)
+#     print(product2.quantity)
--- tests/test_main.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-@pytest.fixture
-def product():
-    return Developer('Ivan', 'Ivanov')
